ae901ff56add67a92d341a64d71f6b68a4c22f87
aski6 <aski6@users.noreply.github.com>
2017-03-25 21:02:11 +0000

    in progress of setting up a working socket

--- source/app.d
+++ source/app.d
@@ -4,4 +4,7 @@ import std.socket;
 void main()
 {
 	writeln("This might be an irc server at some point");
+	TcpSocket serverSocket = new TcpSocket();
+	serverSocket.bind(getAddress("0.0.0.0", 6667)[0]);
+	serverSocket.listen(1);
 }
