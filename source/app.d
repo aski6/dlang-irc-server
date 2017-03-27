b7f98df0a41a21e1a6559ad88b8b84e74b65b961
aski6 <aski6@users.noreply.github.com>
2017-03-27 20:57:21 +0100

    working on network interfaces

--- source/app.d
+++ source/app.d
@@ -7,4 +7,10 @@ void main()
 	TcpSocket serverSocket = new TcpSocket();
 	serverSocket.bind(getAddress("0.0.0.0", 6667)[0]);
 	serverSocket.listen(1);
+	serverSocket.accept();
+	char[50] received;
+	while(true) {
+		serverSocket.receive(received);
+		writeln(received);
+	}
 }
