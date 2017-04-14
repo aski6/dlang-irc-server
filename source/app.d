181600bbb4d7e9508ffd70653942fd76ff071c27
aski6 <aski6@users.noreply.github.com>
2017-04-14 18:51:11 +0100

    Handling of multiple connections code complete.
    Program now prints ascii sent over tcp connections, and can handle several connections, maximum set in config.d currently.
    Code used to handle multiple (non-blocking) sockets is based on https://github.com/dlang/dmd/blob/master/samples/listener.d

--- source/app.d
+++ source/app.d
@@ -1,14 +1,17 @@
 import std.stdio;
 import std.socket;
 import config;
+import std.algorithm.mutation;
 
 void main() {
-	writeln("This might be an irc server at some point");
+	writefln("This might be an irc server at some point");
 	auto listener = new TcpSocket();
+	assert(listener.isAlive);
 	listener.blocking = false;
 	listener.bind(new InternetAddress(ADDR, PORT));
 	listener.listen(1);
-	writeln("Listening for incoming connections on address %s, port %d.", ADDR, PORT);
+	writefln("Listening for incoming connections on address %s, port %d.", ADDR, PORT);
+	writefln("");
 
 	auto socketSet = new SocketSet(MAX_CONNECTIONS + 1); // +1 leaves room for the listener socket.
 	Socket[] connections;
@@ -16,18 +19,55 @@ void main() {
 	while (true) {
 		socketSet.add(listener);
 
-		foreach (socket; connections) {
-			socketSet.add(socket);
+		foreach (connection; connections) {
+			socketSet.add(connection); //add all connections to socketSet to be checked for status chages.
 		}
 
-		Socket.select(socketSet, null, null);
+		Socket.select(socketSet, null, null);  //get list of sockets that have changed status.
 
-		for (int i = 0; i < connections.length; i++) { //for each socket
-			if(socketSet.isSet(connections[i])) { //if socket accessed by loop has changed state
-				char[512] buffer; //irc has a maximum message length of 512 chars, including CR-LF ending (2 chars)
-									
+		for (size_t i = 0; i < connections.length; i++) {
+			if (socketSet.isSet(connections[i])) { //if socket being checked has a status update.
+				char[512] buffer; //irc has a maximum message length of 512 chars, including CR-LF ending (2 chars).
+				auto recLen = connections[i].receive(buffer); //recLen stores the length of the data received into the buffer.
+				if (recLen == Socket.ERROR) {
+					writefln("There was an error receiving from the socket. :(");
+				} else if (recLen != 0) {
+					writefln("Received %d bytes from %s: %s", recLen, connections[i].remoteAddress().toString(), buffer[0.. recLen]);
+				} else {
+					try {
+						//try to state address of socket closing, may fail if connections[i] was closed due to an error.
+						writefln("Connection from %s closed.", connections[i].remoteAddress().toString());
+					} catch (SocketException) {
+						writefln("Connection closed.");
+					}
+					connections[i].close();
+					connections = connections.remove(i);
+					i--;
+				}
 			}
 		}
+		if (socketSet.isSet(listener)) { //if there was a connection request.
+			Socket sn = null;
+			scope (failure) {
+				writefln("Error accepting connection");
+				if (sn) {
+					sn.close();
+				}
+			}
+			sn = listener.accept();
+			assert(sn.isAlive);
+			assert(listener.isAlive);
+			if (connections.length < MAX_CONNECTIONS) {
+				writefln("Connection from %s established.", sn.remoteAddress().toString());
+				connections ~= sn;
+			} else {
+				writefln("Rejected connection from %s: max connections already reached.", sn.remoteAddress().toString());
+				sn.close();
+				assert(!sn.isAlive);
+				assert(listener.isAlive);
+			}
+		}
+		socketSet.reset();
 	}
 	/*
 	while(true) {
@@ -38,11 +78,11 @@ void main() {
 				if(num == 0) { //check that some data has actually been received, if not, our nc socket(used for testing receiving of data from socket) is not active, so start wait for another connection.
 					break;
 				}
-				//writeln(num);
+				//writefln(num);
 				write(buffer[0.. num]);
 			}
 		} catch {
-			writeln("there was an error :(");
+			writefln("there was an error :(");
 		}
 	}
 	*/
